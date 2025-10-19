e7cc709d2a4cd4e7ba565231eaa784edab733676
DGarris <garris.dalton@gmail.com>
2025-10-19 09:49:11 -0400

    B: Added caclulator app

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
  
 client = genai.Client(api_key=api_key)
-#user_prompt = sys.argv[1]
+
 #argument parser implementation
 parser =argparse.ArgumentParser(description="A simple argument parser")
 parser.add_argument("--verbose", action="store_true")
@@ -19,16 +19,11 @@ args = parser.parse_args()
 messages = [
     types.Content(role="user", parts=[types.Part(text=args.prompt)]),
 ]
-#check if no prompt provided by user
-
-
-#generate and print the agent response
+#generate the agent response
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=messages
 )
-
-##Add the --verbose flag. if verbose, include the prompt in the print statement, if not, don't include the prompt in the print statement.
-
+#check for -v flag, else just print response
 if args.verbose:
     print(response.text)
     print(f"User prompt: {args.prompt}")
